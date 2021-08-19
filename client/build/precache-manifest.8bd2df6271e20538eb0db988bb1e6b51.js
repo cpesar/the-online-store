27b4de103012fab0c5efeaa792645ae78abf23db
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-19 15:08:00 -0600

    seeded db

---
+++ client/build/precache-manifest.8bd2df6271e20538eb0db988bb1e6b51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086cd2905a45591c3d6d851ab3a7c64b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b16a25b8f3992d67594",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "12036fa60d0530dc73ef",
+    "url": "/static/js/2.2cd0bd0b.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.2cd0bd0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b16a25b8f3992d67594",
+    "url": "/static/js/main.1405f1bd.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
