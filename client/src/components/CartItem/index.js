9898ede77871c6909cda3d86157a432c754f1ed6
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-19 12:35:00 -0600

    added striple typedefs and resolvers

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -25,12 +25,15 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id
       });
+
+      idbPromise('cart', 'delete', { ...item});
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
     }
   };
 
